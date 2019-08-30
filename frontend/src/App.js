359ba756e00e82ff4dc8e05140d908efa8abb449
Gael Bau <gael@Gaels-MacBook-Pro.local>
2019-08-29 19:31:13 -0700

    add toggle nav for mobile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,38 +6,78 @@ import Contact from './contact/contact';
 import Landing from './landing/landing';
 import NotFound from './not-found/not-found';
 
-function App() {
-  return (
-    <Router>
-      <div className="app">
-        <header className="app-header">
-          <nav>
-            <Link to="/" component={Landing}>Home</Link>
-            <Link to="/about" component={About}>About</Link>
-            <Link to="/contact" component={Contact}>Contact</Link>
-          </nav>
-        </header>
-        <main className="app-main">
-          <Switch>
-            <Route exact path="/" component={Landing}></Route>
-            <Route path="/about" component={About}></Route>
-            <Route path="/contact" component={Contact}></Route>
-            <Route component={NotFound}></Route>
-          </Switch>
-        </main>
-        <footer className="app-footer">
-          <div className="app-footer-item">
-            <a href="/contact">Contact</a>
-          </div>
-          <div className="app-footer-item">
-            <a className="app-footer-social-item" href="https://m.me/GaelCalmaBau"><i class="fab fa-facebook-messenger"></i></a>
-            <a className="app-footer-social-item" href="https://www.linkedin.com/in/gael-bau-97a2b6bb/"><i class="fab fa-linkedin-in"></i></a>
-          </div>
-          <div className="app-footer-item">copyright by me 2019.</div>
-        </footer>
-      </div>
-    </Router>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isNavCollapsed: false
+    }
+    this.timeOutId = null;
+
+    this.collapseNav = this.collapseNav.bind(this);
+    this.onBlurHandler = this.onBlurHandler.bind(this);
+    this.onFocusHandler = this.onFocusHandler.bind(this);
+  }
+
+  collapseNav() {
+    this.setState({
+      isNavCollapsed: !this.state.isNavCollapsed
+    });
+  }
+
+  onBlurHandler() {
+    this.timeOutId = setTimeout(() => {
+      this.setState({
+        isNavCollapsed: false
+      });
+    });
+  }
+
+  onFocusHandler() {
+    clearTimeout(this.timeOutId);
+  }
+
+  render() {
+    const navCollapsed = this.state.isNavCollapsed ? "nav--collapsed" : "nav";
+
+    return (
+      <Router>
+        <div className="app">
+          <header className="app-header" 
+            onBlur={this.onBlurHandler}
+            onFocus={this.onFocusHandler}>
+            <button className="nav--toggle" onClick={this.collapseNav}>
+              <i className="fas fa-bars"></i>
+            </button>
+            <nav className={navCollapsed}>
+              <Link className="nav__item" to="/" onClick={this.onBlurHandler}>Home</Link>
+              <Link className="nav__item" to="/about" onClick={this.onBlurHandler}>About</Link>
+              <Link className="nav__item" to="/contact" onClick={this.onBlurHandler}>Contact</Link>
+            </nav>
+          </header>
+          <main className="app-main">
+            <Switch>
+              <Route exact path="/" component={Landing}></Route>
+              <Route path="/about" component={About}></Route>
+              <Route path="/contact" component={Contact}></Route>
+              <Route component={NotFound}></Route>
+            </Switch>
+          </main>
+          <footer className="app-footer">
+            <div className="app-footer-item">
+              <a href="/contact">Contact</a>
+            </div>
+            <div className="app-footer-item">
+              <a className="app-footer-social-item" href="https://m.me/GaelCalmaBau"><i className="fab fa-facebook-messenger"></i></a>
+              <a className="app-footer-social-item" href="https://www.linkedin.com/in/gael-bau-97a2b6bb/"><i className="fab fa-linkedin-in"></i></a>
+            </div>
+            <div className="app-footer-item">copyright by me 2019.</div>
+          </footer>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
